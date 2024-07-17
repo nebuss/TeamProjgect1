2cca50b02b740d471f2a1ec1a2a39b9978ee32a8
yongraegod <ohhyunwok@naver.com>
2024-07-17 21:35:45 +0900

    commit

--- oh.py
+++ oh.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-# 데이터 불러오기
+# 1. 데이터 불러오기
+
 # 원본 데이터(excel)상, 1행과 2행 일부 부분이 병합되어 있음. 
 # 2행부터 데이터를 받고자 header = 1로 설정하여 데이터 읽음. 
 df = pd.read_excel('data/domestic_2022.xlsx', header = 1)
 df.head()
 
 
-# 데이터 변수 확인 
+# 데이터 변수 확인
 df.columns
 
 
-# 변수 이름 바꿔보기
+# 2. 변수 이름 바꿔보기
 df = df.rename(columns = {'통계분류(1)' : 'class1',
                           '통계분류(2)' : 'class2',
                           '국내전체'    : 'dm_total',
@@ -25,59 +27,66 @@ df = df.rename(columns = {'통계분류(1)' : 'class1',
                           '기타숙박'    : 'ot_stay',
                           '기타당일'    : 'ot_1day'
                           })
-df #df 잘 바뀌었는지 확인
 
-# 'class1'의 Nan 값에 class에 맞는 값 부여
-df.loc[2, ['class1']] = "성별"
-df.loc[4:9, ['class1']] = "연령"
-df.loc[11:16, ['class1']] = "직업"
-df.loc[18:20, ['class1']] = "학력"
-df.loc[22:23, ['class1']] = "가구원수"
-df.loc[25:30, ['class1']] = "가구소득"
+#변수명 잘 바뀌었는지 확인
+df 
 
+# 3. 'class1'의 Nan 값에 class에 맞는 값 부여 & 영문명으로 변경
+df.loc[0, ['class1']] = "categoty-sum"
+df.loc[1:2, ['class1']] = "sex"
+df.loc[3:9, ['class1']] = "age"
+df.loc[10:16, ['class1']] = "job"
+df.loc[17:20, ['class1']] = "edu"
+df.loc[21:23, ['class1']] = "fam"
+df.loc[24:30, ['class1']] = "income"
+df
 
-# 행렬 바꿔서 이름 부여
-df2 = df.transpose()
-df2
-df2.columns
-
-
-#rename 코드 짧게 짤 수 있는 분은 수정 부탁드립니다.
-
-df2 = df2.rename(columns = {0  : 'category'})
-df2 = df2.rename(columns = {1  : 'sex'})
-df2 = df2.rename(columns = {2  : 'sex'})
-df2 = df2.rename(columns = {3  : 'age',
-                            4  : 'age',
-                            5  : 'age',
-                            6  : 'age',
-                            7  : 'age',
-                            8  : 'age',
-                            9  : 'age',
-                            10 : 'job',
-                            11 : 'job',
-                            12 : 'job',
-                            13 : 'job',
-                            15 : 'job',
-                            16 : 'job',
-                            17 : 'edu',
-                            18 : 'edu',
-                            19 : 'edu',
-                            20 : 'edu',
-                            21 : 'fam',
-                            22 : 'fam',
-                            23 : 'fam',
-                            24 : 'income',
-                            25 : 'income',
-                            26 : 'income',
-                            27 : 'income',
-                            28 : 'income',
-                            29 : 'income',
-                            30 : 'income'})
-                            
-#df2에 잘 반영되었는지 확인
-df2
-
-#df_new로 그냥 행렬 바꿔서 보고 싶었음
-df_new = df2.transpose()
+# 분석할 변수인 sex, age, income를 가진 데이터 추출 
+df_new = df.query('class1 == ["sex", "age", "income"]')
 df_new
+
+# 4. 'class1'을 df_new의 index로 설정
+#df_new = df_new.set_index(['class1'])
+
+# 5. 'class2'국문 내용을 영문으로 변경
+#sex
+df_new.loc[df_new["class2"] == "남자", "class2"] = "M"
+df_new.loc[df_new["class2"] == "여자", "class2"] = "F"
+
+#age
+df_new.loc[df_new["class2"] == "15~19세", "class2"] = "10s"
+df_new.loc[df_new["class2"] == "20대", "class2"] = "20s"
+df_new.loc[df_new["class2"] == "30대", "class2"] = "30s"
+df_new.loc[df_new["class2"] == "40대", "class2"] = "40s"
+df_new.loc[df_new["class2"] == "50대", "class2"] = "50s"
+df_new.loc[df_new["class2"] == "60대", "class2"] = "60s"
+df_new.loc[df_new["class2"] == "70세 이상", "class2"] = "70s~"
+
+#income
+df_new.loc[df_new["class2"] == "100만원 미만", "class2"] = "~100"
+df_new.loc[df_new["class2"] == "100~200만원 미만", "class2"] = "100~200"
+df_new.loc[df_new["class2"] == "200~300만원 미만", "class2"] = "200~300"
+df_new.loc[df_new["class2"] == "300~400만원 미만", "class2"] = "300~400"
+df_new.loc[df_new["class2"] == "400~500만원 미만", "class2"] = "400~500"
+df_new.loc[df_new["class2"] == "500~600만원 미만", "class2"] = "500~600"
+df_new.loc[df_new["class2"] == "600만원 이상", "class2"] = "600~"
+
+df_new
+
+
+# 6. age별 dm_total 그래프로 확인해 보기
+df_new.loc[[df_new.query('class1 == "age"')], 'dm_total']
+a = df_new.query('class1 == "age"')
+
+import seaborn as sns
+sns.barplot(data = a, x = 'class2', y = 'dm_total')
+dm_total_age.plot.bar(rot = 0)
+plt.show()
+plt.clf()
+
+#dm_total 내림차순으로 정렬
+a = a.sort_values('dm_total', ascending = False)
+
+dm_total_age.plot.bar(rot = 0)
+plt.show()
+plt.clf()
